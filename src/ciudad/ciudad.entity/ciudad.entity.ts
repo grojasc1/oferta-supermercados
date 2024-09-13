0972d5ba0ceee75898d234d13cbdbcba7020aafb
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-13 18:07:27 -0500

    feat: Add properties to CiudadEntity class
    Add id, nombre, pais, and num_habitantes properties to the CiudadEntity class in ciudad.entity.ts.

--- src/ciudad/ciudad.entity/ciudad.entity.ts
+++ src/ciudad/ciudad.entity/ciudad.entity.ts
@@ -1,1 +1,16 @@
-export class CiudadEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class CiudadEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    pais: string;
+
+    @Column()
+    num_habitantes: number;
+}
