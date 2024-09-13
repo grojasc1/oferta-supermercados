ce067475462636e46eaa7e85fa47ec3bf55de407
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-13 18:11:11 -0500

    feat: Add properties to SupermercadoEntity class

--- src/supermercado/supermercado.entity/supermercado.entity.ts
+++ src/supermercado/supermercado.entity/supermercado.entity.ts
@@ -1,1 +1,19 @@
-export class SupermercadoEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class SupermercadoEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    longitud: number;
+
+    @Column()
+    latitud: number;
+
+    @Column()
+    paginaWeb: string;
+}
