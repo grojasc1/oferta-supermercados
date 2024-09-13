0c78557f520b2cb7871c6a6a7f74456faf6cd508
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-13 18:54:53 -0500

    chore: Update package.json dependencies and add pg package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
 import { SupermercadoModule } from './supermercado/supermercado.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CiudadEntity } from './ciudad/ciudad.entity/ciudad.entity';
+import { SupermercadoEntity } from './supermercado/supermercado.entity/supermercado.entity';
 
 @Module({
-  imports: [CiudadModule, SupermercadoModule],
+  imports: [CiudadModule, SupermercadoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'ofertas',
+      entities: [CiudadEntity, SupermercadoEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
