e4d9e861273138b7e0310c6b00624c80a5d7e990
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-13 19:37:28 -0500

    CRUD supermercado

--- src/supermercado/supermercado.service.ts
+++ src/supermercado/supermercado.service.ts
@@ -2,11 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SupermercadoEntity } from './supermercado.entity/supermercado.entity';
 import { Repository } from 'typeorm';
+import { BusinessError, BusinessLogicException } from 'src/shared/errors/business-errors';
 
 @Injectable()
 export class SupermercadoService {
+
     constructor(
         @InjectRepository(SupermercadoEntity)
         private readonly supermercadoRepository: Repository<SupermercadoEntity>
     ) {}
+
+    async findAll(): Promise<SupermercadoEntity[]> {
+        return await this.supermercadoRepository.find({relations: ['ciudades']});
+    }
+
+    async findOne(id: string): Promise<SupermercadoEntity> {
+        const supermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({where: {id}, relations: ['ciudades']});
+        if (!supermercado)
+            throw new BusinessLogicException("El supermercado con el id proporcionado no existe", BusinessError.NOT_FOUND);
+        return supermercado;
+    }
+
+    async create(supermercado: SupermercadoEntity): Promise<SupermercadoEntity> {
+        if (supermercado.nombre.length <= 10) {
+            throw new BusinessLogicException("El nombre del supermercado debe tener más de 10 caracteres", BusinessError.PRECONDITION_FAILED);
+        }
+
+        return await this.supermercadoRepository.save(supermercado);
+    }
+
+    async update(id: string, supermercado: SupermercadoEntity): Promise<SupermercadoEntity> {
+        const persistedSupermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({where: {id}});
+        if (!persistedSupermercado)
+            throw new BusinessLogicException("El supermercado con el id proporcionado no existe", BusinessError.NOT_FOUND);
+        else if (supermercado.nombre.length <= 10)
+            throw new BusinessLogicException("El nombre del supermercado debe tener más de 10 caracteres", BusinessError.PRECONDITION_FAILED);
+
+        supermercado.id = id;
+
+        return await this.supermercadoRepository.save({...persistedSupermercado, ...supermercado});
+    }
 }
