1e3234eb8239fd5a2c70fc16bb86b451a7a01c94
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-13 19:07:35 -0500

    refactor: Add TypeOrmModule import and CiudadEntity to CiudadModule providers
    Add the TypeOrmModule import and CiudadEntity to the providers array in the CiudadModule file. This allows the CiudadService to use the CiudadEntity for database operations.

--- src/ciudad/ciudad.service.ts
+++ src/ciudad/ciudad.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CiudadEntity } from './ciudad.entity/ciudad.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CiudadService {}
+export class CiudadService {
+    constructor(
+        @InjectRepository(CiudadEntity)
+        private readonly ciudadRepository: Repository<CiudadEntity>
+    ){}
+}
