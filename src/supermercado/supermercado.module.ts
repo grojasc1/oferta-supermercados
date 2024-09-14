d7d57d23a6c83b0b9d4cafdcd036f97c1f1a52ac
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-13 19:09:28 -0500

    refactor: Add TypeOrmModule import and SupermercadoEntity to SupermercadoModule providers
    Add the TypeOrmModule import and SupermercadoEntity to the providers array in the SupermercadoModule file.

--- src/supermercado/supermercado.module.ts
+++ src/supermercado/supermercado.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SupermercadoService } from './supermercado.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupermercadoEntity } from './supermercado.entity/supermercado.entity';
 
 @Module({
-  providers: [SupermercadoService]
+  providers: [SupermercadoService],
+  imports: [TypeOrmModule.forFeature([SupermercadoEntity])],
 })
 export class SupermercadoModule {}
--- src/supermercado/supermercado.service.ts
+++ src/supermercado/supermercado.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SupermercadoEntity } from './supermercado.entity/supermercado.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SupermercadoService {}
+export class SupermercadoService {
+    constructor(
+        @InjectRepository(SupermercadoEntity)
+        private readonly supermercadoRepository: Repository<SupermercadoEntity>
+    ) {}
+}
