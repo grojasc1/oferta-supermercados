0c2ebf61e655234b49afe52831d1058a232d2c39
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-14 17:26:59 -0500

    refactor: Update package.json dependencies and add faker library

--- src/ciudad/ciudad.service.spec.ts
+++ src/ciudad/ciudad.service.spec.ts
@@ -4,6 +4,7 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { CiudadService } from '../ciudad/ciudad.service';
 import { CiudadEntity } from '../ciudad/ciudad.entity/ciudad.entity';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { faker } from '@faker-js/faker';
 
 describe('CiudadService', () => {
   let service: CiudadService;
@@ -18,8 +19,21 @@ describe('CiudadService', () => {
 
     service = module.get<CiudadService>(CiudadService);
     repository = module.get<Repository<CiudadEntity>>(getRepositoryToken(CiudadEntity));
-    //await seedDatabase();
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    ciudadesList = [];
+    for (let i = 0; i < 5; i++) {
+      const ciudad: CiudadEntity = await repository.save({
+        nombre: faker.address.city(),
+        pais: faker.address.country(),
+        num_habitantes: faker.number.int({ min: 1000, max: 1000000 }),
+      });
+      ciudadesList.push(ciudad);
+    }
+  }
 
   it('should be defined', () => {
     expect(service).toBeDefined();
