476be011c5a977e62e0fde8dadd621bdec305767
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-14 19:45:53 -0500

    feat: Add CiudadController and SupermercadoController

--- src/ciudad/ciudad.module.ts
+++ src/ciudad/ciudad.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CiudadService } from './ciudad.service';
 import { CiudadEntity } from './ciudad.entity/ciudad.entity';
+import { CiudadController } from './ciudad.controller';
 
 @Module({
   providers: [CiudadService],
   imports: [TypeOrmModule.forFeature([CiudadEntity])],
+  controllers: [CiudadController],
 })
 export class CiudadModule {}
--- src/supermercado/supermercado.module.ts
+++ src/supermercado/supermercado.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupermercadoService } from '../supermercado/supermercado.service';
 import { SupermercadoEntity } from '../supermercado/supermercado.entity/supermercado.entity';
+import { SupermercadoController } from './supermercado.controller';
 
 @Module({
   providers: [SupermercadoService],
   imports: [TypeOrmModule.forFeature([SupermercadoEntity])],
+  controllers: [SupermercadoController],
 })
 export class SupermercadoModule {}
