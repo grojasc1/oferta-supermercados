ef10af8a647ffab8bd42ebb376e7c5693efff018
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-14 19:53:36 -0500

    refactor: Add BusinessErrorsInterceptor to CiudadController and SupermercadoController

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { CiudadService } from './ciudad.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
 @Controller('cities')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class CiudadController {
     constructor(
         private readonly ciudadService: CiudadService,
--- src/supermercado/supermercado.controller.ts
+++ src/supermercado/supermercado.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { SupermercadoService } from './supermercado.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
 @Controller('supermarkets')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class SupermercadoController {
     constructor(
         private readonly supermercadoService: SupermercadoService
