a8843b57bcfd507853d0d00ce321327de0631ba7
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-14 20:08:55 -0500

    feat: Add class-transformer package and update CiudadDto and SupermercadoDto classes

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
 import { CiudadService } from './ciudad.service';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
+import { CiudadDto } from './ciudad.dto/ciudad.dto';
+import { CiudadEntity } from './ciudad.entity/ciudad.entity';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('cities')
 @UseInterceptors(BusinessErrorsInterceptor)
@@ -8,4 +11,32 @@ export class CiudadController {
     constructor(
         private readonly ciudadService: CiudadService,
     ) {}
+
+    @Get()
+    async findAll() {
+        return await this.ciudadService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return await this.ciudadService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() ciudadDto: CiudadDto) {
+        const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
+        return await this.ciudadService.create(ciudad);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() ciudadDto: CiudadDto) {
+        const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
+        return await this.ciudadService.update(id, ciudad);
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    async delete(@Param('id') id: string) {
+        return await this.ciudadService.delete(id);
+    }
 }
