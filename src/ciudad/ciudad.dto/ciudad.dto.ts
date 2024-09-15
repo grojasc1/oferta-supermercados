886a11af9bb916011c7234e725ab74cd16f9eb65
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-14 20:02:15 -0500

    feat: Add class-validator package and update CiudadDto and SupermercadoDto classes

--- src/ciudad/ciudad.dto/ciudad.dto.ts
+++ src/ciudad/ciudad.dto/ciudad.dto.ts
@@ -1,1 +1,15 @@
-export class CiudadDto {}
+import {IsNotEmpty, IsNumber, IsString} from 'class-validator';
+export class CiudadDto {
+
+    @IsString()
+    @IsNotEmpty()
+    readonly nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly pais: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    readonly num_habitantes: number;
+}
--- src/supermercado/supermercado.dto/supermercado.dto.ts
+++ src/supermercado/supermercado.dto/supermercado.dto.ts
@@ -1,1 +1,20 @@
-export class SupermercadoDto {}
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from "class-validator";
+
+export class SupermercadoDto {
+
+    @IsString()
+    @IsNotEmpty()
+    readonly nombre: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    readonly longitud: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    readonly latitud: number;
+
+    @IsUrl()
+    @IsNotEmpty()
+    readonly paginaWeb: string;
+}
