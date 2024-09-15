d53b54202c3027f94ed2e3dc2806fa1f56c67f7d
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-14 20:11:32 -0500

    refactor: Update SupermercadoController with class-transformer and class-validator packages

--- src/supermercado/supermercado.controller.ts
+++ src/supermercado/supermercado.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
 import { SupermercadoService } from './supermercado.service';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
+import { SupermercadoDto } from './supermercado.dto/supermercado.dto';
+import { plainToInstance } from 'class-transformer';
+import { SupermercadoEntity } from './supermercado.entity/supermercado.entity';
 
 @Controller('supermarkets')
 @UseInterceptors(BusinessErrorsInterceptor)
@@ -8,4 +11,32 @@ export class SupermercadoController {
     constructor(
         private readonly supermercadoService: SupermercadoService
     ) {}
+
+    @Get()
+    async findAll() {
+        return await this.supermercadoService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return await this.supermercadoService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() supermercadoDto: SupermercadoDto) {
+        const supermercado = plainToInstance(SupermercadoEntity, supermercadoDto);
+        return await this.supermercadoService.create(supermercado);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() supermercadoDto: SupermercadoDto) {
+        const supermercado = plainToInstance(SupermercadoEntity, supermercadoDto);
+        return await this.supermercadoService.update(id, supermercado);
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    async delete(@Param('id') id: string) {
+        return await this.supermercadoService.delete(id);
+    }
 }
